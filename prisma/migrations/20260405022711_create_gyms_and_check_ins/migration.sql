b7c381093be8aa816adcc42cff8e5b1d6caa2913
Afranio Caires <afraniomcaires@gmail.com>
2026-04-07 21:38:58 -0300

    feat: add user, check_in, and gym models with relationships

---
+++ prisma/migrations/20260405022711_create_gyms_and_check_ins/migration.sql
@@ -0,0 +1,35 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password_hash" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "check_ins" (
+    "id" TEXT NOT NULL,
+    "validated_at" TIMESTAMP(3),
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "check_ins_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "gyms" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "phone" TEXT,
+    "latitude" DOUBLE PRECISION NOT NULL,
+    "longitude" DOUBLE PRECISION NOT NULL,
+
+    CONSTRAINT "gyms_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
