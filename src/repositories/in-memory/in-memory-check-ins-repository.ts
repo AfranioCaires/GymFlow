2bf549efbecdf40078f520fb563e98e2f12fd107
Afranio Caires <afraniomcaires@gmail.com>
2026-04-12 01:10:36 -0300

    feat: implement findByUserIdOnDate method in CheckInsRepository for in-memory and Prisma implementations

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -20,4 +20,22 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
 
     return Promise.resolve(checkIn)
   }
+
+  async findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null> {
+    const startOfTheDay = new Date(date)
+    startOfTheDay.setHours(0, 0, 0, 0)
+
+    const endOfTheDay = new Date(date)
+    endOfTheDay.setHours(23, 59, 59, 999)
+
+    const checkInOnSameDate = this.checkIns.find((checkIn) => {
+      return (
+        checkIn.user_id === userId &&
+        checkIn.created_at >= startOfTheDay &&
+        checkIn.created_at <= endOfTheDay
+      )
+    })
+
+    return checkInOnSameDate || null
+  }
 }
--- src/repositories/prisma/prisma-check-ins-repositpry.ts
+++ src/repositories/prisma/prisma-check-ins-repositpry.ts
@@ -8,4 +8,22 @@ export class PrismaCheckInsRepository implements CheckInsRepository {
   async create(data: CheckInUncheckedCreateInput): Promise<CheckIn> {
     return await prisma.checkIn.create({ data })
   }
+
+  async findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null> {
+    const startOfTheDay = new Date(date)
+    startOfTheDay.setHours(0, 0, 0, 0)
+
+    const endOfTheDay = new Date(date)
+    endOfTheDay.setHours(23, 59, 59, 999)
+
+    return await prisma.checkIn.findFirst({
+      where: {
+        user_id: userId,
+        created_at: {
+          gte: startOfTheDay,
+          lte: endOfTheDay,
+        },
+      },
+    })
+  }
 }
