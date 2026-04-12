2288112a843a79acd2132c11c672a1fab8c16b55
Afranio Caires <afraniomcaires@gmail.com>
2026-04-12 14:50:07 -0300

    refactor: change DateUtils from class to object for improved readability

--- src/util/date-utils.ts
+++ src/util/date-utils.ts
@@ -1,21 +1,21 @@
-export class DateUtils {
-  static startOfTheDay(date: Date) {
+export const DateUtils = {
+  startOfTheDay(date: Date) {
     const copy = new Date(date)
     copy.setHours(0, 0, 0, 0)
     return copy
-  }
+  },
 
-  static endOfTheDay(date: Date) {
+  endOfTheDay(date: Date) {
     const copy = new Date(date)
     copy.setHours(23, 59, 59, 999)
     return copy
-  }
+  },
 
-  static isOnTheSameDay(dateA: Date, dateB: Date) {
+  isOnTheSameDay(dateA: Date, dateB: Date) {
     return this.startOfTheDay(dateA).getTime() === this.startOfTheDay(dateB).getTime()
-  }
+  },
 
-  static isBetween(data: { date: Date; startDate: Date; endDate: Date; inclusive?: boolean }) {
+  isBetween(data: { date: Date; startDate: Date; endDate: Date; inclusive?: boolean }) {
     const { date, startDate, endDate, inclusive } = data
 
     if (inclusive) {
@@ -23,5 +23,5 @@ export class DateUtils {
     }
 
     return date > startDate && date < endDate
-  }
+  },
 }
