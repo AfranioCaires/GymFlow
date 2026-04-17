34971e44ba139ae25c4a8defe10d672bdda141d9
Afranio Caires <afraniomcaires@gmail.com>
2026-04-17 00:30:28 -0300

    feat: implement PrismaCheckInsRepository with create, findByUserIdOnDate, and findManyByUserId methods

--- src/repositories/prisma/prisma-check-ins-repository.ts
+++ src/repositories/prisma/prisma-check-ins-repository.ts
@@ -1,3 +1,4 @@
+import { PAGINATION_DEFAULT_PAGE_SIZE } from '@/config/pagination'
 import type { CheckIn } from '@/generated/prisma/browser'
 import type { CheckInUncheckedCreateInput } from '@/generated/prisma/models'
 import { prisma } from '@/lib/prisma'
@@ -22,6 +23,23 @@ export class PrismaCheckInsRepository implements CheckInsRepository {
           lte: endOfTheDay,
         },
       },
+    })
+  }
+
+  async findManyByUserId(data: {
+    userId: string
+    page?: number
+    limit?: number
+  }): Promise<CheckIn[]> {
+    const currentPage = data.page ?? 1
+    const itemsPerPage = data.limit ?? PAGINATION_DEFAULT_PAGE_SIZE
+
+    return prisma.checkIn.findMany({
+      where: {
+        user_id: data.userId,
+      },
+      take: itemsPerPage,
+      skip: (currentPage - 1) * itemsPerPage,
     })
   }
 }
