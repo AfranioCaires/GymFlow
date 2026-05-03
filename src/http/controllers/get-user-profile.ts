086c7f6e955df39aef9f2ef79dd00935a4539170
Afranio Caires <afraniomcaires@gmail.com>
2026-05-03 18:23:19 -0300

    refactor: simplify getUserProfileController by removing schema validation

--- src/http/controllers/get-user-profile.ts
+++ src/http/controllers/get-user-profile.ts
@@ -1,22 +1,16 @@
 import type { FastifyReply, FastifyRequest } from 'fastify'
-import { z } from 'zod'
 
 import { ResourceNotFoundError } from '@/use-cases/errors/resource-not-found-error'
 import { GetUserProfileUseCaseFactory } from '@/use-cases/factories/make-get-user-profile-use-case'
 import { catchError } from '@/util/error-catcher'
 
 export async function getUserProfileController(request: FastifyRequest, reply: FastifyReply) {
-  const getUserProfileBodySchema = z.object({
-    userId: z.uuid(),
-  })
-
   const getUserProfileUseCase = GetUserProfileUseCaseFactory.create()
 
-  const data = getUserProfileBodySchema.parse(request.body)
-
-  const [error, user] = await catchError(getUserProfileUseCase.execute(data), [
-    ResourceNotFoundError,
-  ])
+  const [error, user] = await catchError(
+    getUserProfileUseCase.execute({ userId: request.user.sub }),
+    [ResourceNotFoundError],
+  )
 
   if (error) {
     return reply.status(404).send({ message: error.message })
